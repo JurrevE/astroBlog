4048b678fdea6d27b0ebf1616eb8dcdf4c70ed74
JurreWindows <jurreveijk13@gmail.com>
2023-12-14 14:05:14 +0100

    almost done

---
+++ src/scripts/menu.js
@@ -0,0 +1,4 @@
+document.querySelector('.hamburger').addEventListener('click', () => {
+    console.log("Krijg kanker")
+    document.querySelector('.nav-links').classList.toggle('expanded');
+  });
